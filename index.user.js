acf1cce9b2eec84eb9ab119803e9452c61f610d6
oezeb <eouedraogo4@gmail.com>
2024-03-19 19:37:18 +0800

    table headers in english

--- index.user.js
+++ index.user.js
@@ -43,15 +43,15 @@
         table.style.overflow = "auto";
 
         let columns = {
-            BGTMZW: { name: "报告题目（中文）", size: "300px" },
-            BGTMYW: { name: "报告题目（英文）", size: "158px" },
-            BGRZW: { name: "报告人", size: "100px" },
-            YXDM_DISPLAY: { name: "院系", size: "100px" },
-            DD: { name: "地点", size: "100px" },
-            BGSJ: { name: "报告时间", size: "130px" },
-            JZSJ: { name: "选课截至时间", size: "130px" },
-            "KXRS/YXRS": { name: "容量/已选", size: "80px" },
-            BZ: { name: "备注", size: "100px" },
+            BGTMZW: { name: "Title (Chinese)", size: "300px" },
+            BGTMYW: { name: "Title (English)", size: "158px" },
+            BGRZW: { name: "Speaker", size: "100px" },
+            YXDM_DISPLAY: { name: "Department", size: "100px" },
+            DD: { name: "Location", size: "100px" },
+            BGSJ: { name: "Time", size: "130px" },
+            JZSJ: { name: "Deadline", size: "130px" },
+            "YXRS/KXRS": { name: "Capacity/Selected", size: "80px" },
+            BZ: { name: "Note", size: "100px" },
         };
 
         table.appendChild(createTableHeader(columns));
@@ -85,8 +85,8 @@
         let tr = document.createElement("tr");
         for (let column in columns) {
             let text =
-                column == "KXRS/YXRS"
-                    ? `${item.KXRS}/${item.YXRS}`
+                column == "YXRS/KXRS"
+                    ? `${item.YXRS}/${item.KXRS}`
                     : item[column];
             let td = createTableCell(text, columns[column].size);
             tr.appendChild(td);
@@ -128,8 +128,7 @@
         let iframe = document.createElement("iframe");
         iframe.id = "xsbgxk_iframe";
         iframe.style.display = "none";
-        iframe.src =
-            "/gsapp/sys/xsbgglappustc/*default/index.do?v=b072d6cc-e96a-44a3-8395-917e43a3208a&amp;THEME=blue&amp;EMAP_LANG=zh&amp;min=1&amp;_yhz=5257ccc1e6bf40f388a6b8169c80b56d#/xsbgxk";
+        iframe.src = "/gsapp/sys/xsbgglappustc/*default/index.do?#/xsbgxk";
         return iframe;
     };
 
@@ -138,24 +137,7 @@
             "https://yjs1.ustc.edu.cn/gsapp/sys/xsbgglappustc/modules/xsbgxk/wxbgbgdz.do";
 
         let res = await fetch(url, {
-            headers: {
-                accept: "application/json, text/javascript, */*; q=0.01",
-                "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-                "content-type":
-                    "application/x-www-form-urlencoded; charset=UTF-8",
-                "sec-ch-ua":
-                    '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": '"Windows"',
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-origin",
-                "x-requested-with": "XMLHttpRequest",
-                cookie: document.cookie,
-                Referer:
-                    "https://yjs1.ustc.edu.cn/gsapp/sys/xsbgglappustc/*default/index.do?v=c85d55a2-7d7a-4375-9450-39d14585e32d&THEME=blue&EMAP_LANG=zh&min=1&_yhz=5257ccc1e6bf40f388a6b8169c80b56d",
-                "Referrer-Policy": "strict-origin-when-cross-origin",
-            },
+            headers: { cookie: document.cookie },
             body: `*order=-BGSJ&pageSize=${n}&pageNumber=1`,
             method: "POST",
         });
